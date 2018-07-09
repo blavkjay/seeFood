83793964e8f5c219d75942f5d29a10491cd4119c
OLAJUWON <BADGUY@jays-macbook-pro.local>
2018-07-09 13:01:05 +0100

    Initial Commit

---
+++ seeFood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  seeFood
+//
+//  Created by OLAJUWON on 7/9/18.
+//  Copyright © 2018 OLAJUWON. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
